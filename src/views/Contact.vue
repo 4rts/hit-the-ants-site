542ee6346d2d7544fa7e8670de27c926e32946c6
minjae <alswo2722@naver.com>
2020-03-15 02:47:00 +0900

    Contact outline 완료

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,12 +1,51 @@
 <template>
-    <div>
-        ssss
-    </div>
+    <v-form
+        ref="form"
+        v-model="valid"
+        :lazy-validation="lazy"
+    >
+        <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="subject"
+            :rules="nameRules"
+            label="Subject"
+            required
+        ></v-text-field>
+        <v-textarea
+          solo
+          name="input-7-4"
+          label="Solo textarea"
+        ></v-textarea>
+
+        <v-btn text large color="black">Submit</v-btn>
+    </v-form>
 </template>
 
 <script>
 export default {
-    name: 'Contact'
+    name: 'Contact',
+    data: function () {
+        return {
+            subject: '',
+            // nameRules: [
+            //     v => !!v || 'Subject is required',
+            //     v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            // ],
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 
