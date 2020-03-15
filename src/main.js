b85ab18766c88c0e84d0e086d5d56ad18b9d05b3
minjae <alswo2722@naver.com>
2020-03-15 19:17:10 +0900

    경로 변경

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import style from './assets/css/main.scss'
+import styles from './assets/styles/main.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
-  style,
+  styles,
   render: h => h(App)
 }).$mount('#app')
