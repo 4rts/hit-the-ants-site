1e04a356a869dd376d9932de1d657ef77e3426e1
minjae <alswo2722@naver.com>
2020-03-15 18:16:19 +0900

    헤더뷰 레이아웃

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import sytles from './assets/css/index.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  sytles,
   render: h => h(App)
 }).$mount('#app')
