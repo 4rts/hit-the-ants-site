10f5b6ff48237bb603e42e1d580d1ae551a306f3
minjae <alswo2722@naver.com>
2020-03-17 21:15:20 +0900

    merge conflict

--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
-<<<<<<< HEAD
-import styles from './assets/styles/main.scss'
-=======
 import style from './assets/styles/variables.scss'
->>>>>>> 842c57140d8aa7fbdc3f686c783f215ee912391f
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
-  styles,
+  style,
   render: h => h(App)
 }).$mount('#app')
