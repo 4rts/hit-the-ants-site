a38908c114fb55571402e55f2935595c25c8a2af
hdson07 <sonheeduck@naver.com>
2020-03-17 20:15:12 +0900

    apply Font

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import style from './assets/css/main.scss'
+import style from './styles/variables.scss'
 
 Vue.config.productionTip = false
 
